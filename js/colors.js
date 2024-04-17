b8090840c115a989ce1082984da714c1039c22a8
andyzheng679 <andyzheng679@gmail.com>
2024-04-16 23:12:44 -0400

    Finished

--- js/colors.js
+++ js/colors.js
@@ -1,10 +1,10 @@
-var colorsArray : {
-    colors=[
+var colorsArray = {
+    colors: [
         {
             "name":"green",
             "red":"64",
             "green":"175",
-            "blue":"63",
+            "blue":"63"
         },
         {
             "name":"yellow",
@@ -17,17 +17,18 @@ var colorsArray : {
             red:"192",
             green:"46",
             blue:"35"
-        }
+        },
         {
             "name":"blue",
             "red":"19",
             "green":"71",
-            "blue":"144",
+            "blue":"144"
         },
         {
-            "name"="orange",
-            "red"="253",
-            "green"="92",
-            "blue"="48"
-        },
+            "name":"orange",
+            "red":"253",
+            "green":"92",
+            "blue":"48"
+        }
     ]
+    };
